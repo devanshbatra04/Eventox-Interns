59439df7ca8f388ab8d83a613b19570a5ae712f6
Devansh Batra <devanshbatra04@gmail.com>
2018-08-13 10:47:11 +0530

    intern landing page online

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ app.set('view engine', ejs);
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/public', express.static(__dirname + '/public'));
 
-
+app.get('/', function(req, res){
+    res.render('intern/landing.ejs');
+})
 
 app.listen(5002, function(req,res){
     console.log("listening on port " + 5002);
